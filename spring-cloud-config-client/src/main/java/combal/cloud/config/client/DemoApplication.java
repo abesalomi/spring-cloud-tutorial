dfa975918a6855ddafc89b876c5c47f292cfff6c
Abesalomi Gogatishvili <abesalomi.gogatishvili@gmail.com>
2017-03-01 20:03:02 +0400

    Print hello from client and update client configuration.

--- spring-cloud-config-client/src/main/java/combal/cloud/config/client/DemoApplication.java
+++ spring-cloud-config-client/src/main/java/combal/cloud/config/client/DemoApplication.java
@@ -1,12 +1,25 @@
 package combal.cloud.config.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@Value("${hello.to.client}")
+	private String hello;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@RequestMapping("hello")
+	public String hello(){
+		return hello;
+	}
+
 }
